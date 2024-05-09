ba9c5e34e067dd9aa98c2ba5f10587eb9a707bb3
ponatosik <ponatosik@gmail.com>
2024-05-09 11:53:21 +0300

    test: update tests

--- src/main/test/com/ponatosik/kanban/application/ApplicationUnitTest.java
+++ src/main/test/com/ponatosik/kanban/application/ApplicationUnitTest.java
@@ -1,11 +1,14 @@
 package com.ponatosik.kanban.application;
 
+import com.ponatosik.kanban.application.interfaces.UserService;
 import com.ponatosik.kanban.application.repositories.GroupsRepository;
 import com.ponatosik.kanban.application.repositories.StatusRepository;
 import com.ponatosik.kanban.application.repositories.TaskRepository;
+import com.ponatosik.kanban.application.repositories.UserRepository;
 import com.ponatosik.kanban.core.entities.Group;
 import com.ponatosik.kanban.core.entities.Status;
 import com.ponatosik.kanban.core.entities.Task;
+import com.ponatosik.kanban.core.entities.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -13,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -30,11 +34,19 @@ public abstract class ApplicationUnitTest {
     @Mock
     protected TaskRepository taskRepository;
 
+    @Mock
+    protected UserRepository userRepository;
+
+    @Mock
+    protected UserService userService;
+
     @BeforeEach
     void mocksSetUp() {
         var group = Group.createGroup(0, "test group");
         var status = group.createStatus(0, "test status");
         var task = group.createTask(0, "test task", status);
+        var user = new User("test", "test", "test", new ArrayList<>());
+        user.addGroup(group);
 
         when(groupsRepository.findById(any(Integer.class)))
                 .then(x -> Optional.of(group));
@@ -45,8 +57,15 @@ public abstract class ApplicationUnitTest {
         when(taskRepository.findById(any(Integer.class)))
                 .then(x -> Optional.of(task));
 
+        when(userRepository.findById(any(String.class))).
+                then(x -> Optional.of(user));
+
+        when(userService.getAuthenticatedUser()).
+                then(x -> Optional.of(user));
+
         when(groupsRepository.save(any(Group.class))).then(x -> x.getArgument(0));
         when(statusRepository.save(any(Status.class))).then(x -> x.getArgument(0));
         when(taskRepository.save(any(Task.class))).then(x -> x.getArgument(0));
+        when(userRepository.save(any(User.class))).then(x -> x.getArgument(0));
     }
 }
--- src/main/test/com/ponatosik/kanban/application/handlers/GetGroupsQueryHandlerTest.java
+++ src/main/test/com/ponatosik/kanban/application/handlers/GetGroupsQueryHandlerTest.java
@@ -3,13 +3,16 @@ package com.ponatosik.kanban.application.handlers;
 import com.ponatosik.kanban.application.ApplicationUnitTest;
 import com.ponatosik.kanban.application.requests.GetGroupsQuery;
 import com.ponatosik.kanban.core.entities.Group;
+import com.ponatosik.kanban.core.entities.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 class GetGroupsQueryHandlerTest extends ApplicationUnitTest {
@@ -25,6 +28,9 @@ class GetGroupsQueryHandlerTest extends ApplicationUnitTest {
     @BeforeEach
     void setUp() {
         when(groupsRepository.findAll()).then(x -> savedGroups);
+        var savedUser =  Optional.of(new User("test", "test", "test", savedGroups));
+        when(userRepository.findById(any(String.class))).then(x -> savedUser);
+        when(userService.getAuthenticatedUser()).then(x -> savedUser);
     }
 
     @Test
